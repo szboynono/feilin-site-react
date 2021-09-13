eb571d3ac02fa7b9b319eaa190201bb33e295073
szboynono <szboylbd@hotmail.com>
2021-09-13 00:35:22 -0400

    footer fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="grid grid-cols-3">
+    <div className="grid grid-cols-3 gap-8">
       <div className="w-6 h-6 bg-red-600"></div>
       <div className="col-start-3 font-light text-sm justify-self-start align-self-center">
         <p>&#169;2020 By Feilin Fu</p>
